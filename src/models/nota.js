6e4e4d2b1b4a64099a203a45413e4cf91d8cca38
Henrique dos Santos Alves <henriquelp321@gmail.com>
2023-06-29 21:07:34 -0300

    feature #8

--- src/models/nota.js
+++ src/models/nota.js
@@ -15,8 +15,22 @@ class Nota {
     }
 
     mediaCA() {
-        return "SS";
-    }
+		const media = this.mediaFinal();
+
+		if (media >= 9.0 && media <= 10.0) {
+			return 'SS';
+		} else if (media >= 7.0 && media < 9.0) {
+			return 'MS';
+		} else if (media >= 5.0 && media < 7.0) {
+			return 'MM';
+		} else if (media >= 3.0 && media < 5.0) {
+			return 'MI';
+		} else if (media >= 0.1 && media < 3.0) {
+			return 'II';
+		} else {
+			return 'SR';
+		}
+	}
 }
 
 module.exports = Nota;
